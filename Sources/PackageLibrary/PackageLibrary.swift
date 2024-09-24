d2308041e8998cad70351afeb91879ec62888dde
Alexandr Gerasymov <gerasimov@zfort.com>
2024-09-24 12:54:43 +0300

    no message

--- Sources/PackageLibrary/PackageLibrary.swift
+++ Sources/PackageLibrary/PackageLibrary.swift
@@ -5,6 +5,8 @@ import SwiftUI
 import PolyKit
 
 public struct ContentView: View {
+    public init() {}
+    
     public var body: some View {
         Polygon(count: 6, cornerRadius: 60)
             .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
