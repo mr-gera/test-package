78fc4e7a0b54f0ba47717269f444351247f5cc0f
Alexandr Gerasymov <gerasimov@zfort.com>
2024-09-24 12:45:34 +0300

    test add

---
+++ Sources/PackageLibrary/PackageLibrary.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+import PolyKit
+
+struct ContentView: View {
+    var body: some View {
+        Polygon(count: 6, cornerRadius: 60)
+            .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
